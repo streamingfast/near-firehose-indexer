c26005ac9eda7566b08897195642a9b09da93f3e
billettc <charles@cbillette.com>
2021-09-01 14:03:57 -0400

    bring back start_block opts

--- src/main.rs
+++ src/main.rs
@@ -21,14 +21,19 @@ fn main() {
         .home
         .unwrap_or(std::path::PathBuf::from(near_indexer::get_default_home()));
 
+
+    let mut sync_mode = near_indexer::SyncModeEnum::LatestSynced;
     let start_block = opts.start_block.unwrap_or(0);
+    if start_block > 0 {
+        sync_mode = near_indexer::SyncModeEnum::BlockHeight(start_block)
+    }
+
     match opts.subcmd {
         SubCommand::Run => {
             info!(target: "main", "Running");
             let indexer_config = near_indexer::IndexerConfig {
                 home_dir,
-                // FIXME: We should configure that in the flags!
-                sync_mode: near_indexer::SyncModeEnum::LatestSynced,
+                sync_mode,
                 await_for_node_synced: near_indexer::AwaitForNodeSyncedEnum::StreamWhileSyncing,
             };
 
