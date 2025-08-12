14a090dfa14d4dc7e713cd105c544755613b9f91
Matthieu Vachon <matt@streamingfast.io>
2025-08-12 10:12:05 -0400

    Fixed changelog generation

--- .github/generate_change_log.sh
+++ .github/generate_change_log.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
-checksum() {
-    echo $(sha256sum $@ | awk '{print $1}')
-}
 
 change_log_file="./CHANGELOG.md"
-version="## $@"
-version_prefix="## [0-9]{1,2}\."
+section_prefix="^## "
+
 start=0
 CHANGE_LOG=""
 while read line; do
-    if [[ $line == *"$version"* ]]; then
+    if [[ $line =~ $section_prefix ]] && [ $start == 0 ]; then
         start=1
         continue
     fi
-    if [[ $line =~ $version_prefix ]] && [ $start == 1 ]; then
+
+    if [[ $line =~ $section_prefix ]] && [ $start == 1 ]; then
         break;
     fi
+
     if [ $start == 1 ]; then
         CHANGE_LOG+="$line\n"
     fi
@@ -23,7 +22,7 @@ done < ${change_log_file}
 
 OUTPUT=$(cat <<-END
 ## Changelog\n
-${CHANGE_LOG}\n
+${CHANGE_LOG}
 END
 )
 
